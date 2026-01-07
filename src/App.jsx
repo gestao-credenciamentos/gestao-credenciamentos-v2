0764fc3a95918a55ae0e5c70fc99cac1a2707847
Compras <Compras@amv.com>
2026-01-07 08:03:05 -0300

    https://gestao-credenciamentos-v2.vercel.app/login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import Dashboard from "./pages/Dashboard";
 
@@ -6,7 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
